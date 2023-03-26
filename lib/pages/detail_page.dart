9aca9be2a3a83bf7b002f712da4e396d93fe6da6
Muhammadali Mahmudov <wearfitpro2@gmail.com>
2023-03-26 14:05:11 +0500

    Initial commit

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+
+  static const String id = "Detail_Page";
+
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
